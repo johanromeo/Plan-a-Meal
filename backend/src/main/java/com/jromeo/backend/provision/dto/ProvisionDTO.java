1efc7ffad27cbca7e71e3fd0a68b15e8544f6f59
johanromeo <johan_romeo123@live.se>
2024-10-04 20:44:01 +0200

    dto and mapper implemented

--- backend/src/main/java/com/jromeo/backend/provision/dto/ProvisionDTO.java
+++ backend/src/main/java/com/jromeo/backend/provision/dto/ProvisionDTO.java
@@ -1,4 +1,41 @@
 package com.jromeo.backend.provision.dto;
 
 public class ProvisionDTO {
+
+    private String name;
+    private int units;
+    private boolean addedToGroceryShoppingList;
+
+    public ProvisionDTO() {
+    }
+
+    public ProvisionDTO(String name, int units, boolean addedToGroceryShoppingList) {
+        this.name = name;
+        this.units = units;
+        this.addedToGroceryShoppingList = addedToGroceryShoppingList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getUnits() {
+        return units;
+    }
+
+    public void setUnits(int units) {
+        this.units = units;
+    }
+
+    public boolean isAddedToGroceryShoppingList() {
+        return addedToGroceryShoppingList;
+    }
+
+    public void setAddedToGroceryShoppingList(boolean addedToGroceryShoppingList) {
+        this.addedToGroceryShoppingList = addedToGroceryShoppingList;
+    }
 }
--- backend/src/main/java/com/jromeo/backend/provision/mapper/ProvisionMapper.java
+++ backend/src/main/java/com/jromeo/backend/provision/mapper/ProvisionMapper.java
@@ -1,4 +1,27 @@
 package com.jromeo.backend.provision.mapper;
 
+import com.jromeo.backend.provision.dto.ProvisionDTO;
+import com.jromeo.backend.provision.entity.ProvisionEntity;
+
 public class ProvisionMapper {
+
+    public ProvisionDTO mapToDTO(ProvisionEntity provisionEntity) {
+        ProvisionDTO  provisionDTO = new ProvisionDTO(
+                provisionEntity.getName(),
+                provisionEntity.getUnits(),
+                provisionEntity.isAddedToGroceryShoppingList()
+        );
+
+        return provisionDTO;
+    }
+
+    public ProvisionEntity mapToEntity(ProvisionDTO provisionDTO) {
+        ProvisionEntity provisionEntity = new ProvisionEntity(
+                provisionDTO.getName(),
+                provisionDTO.getUnits(),
+                provisionDTO.isAddedToGroceryShoppingList()
+        );
+
+        return provisionEntity;
+    }
 }
