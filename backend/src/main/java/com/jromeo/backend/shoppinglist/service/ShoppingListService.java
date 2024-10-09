2f788f3c076fec537dd0fd1df9fc0cc052a450a1
johanromeo <johan_romeo123@live.se>
2024-10-09 20:07:50 +0200

    shortened shopping list service method

--- backend/src/main/java/com/jromeo/backend/shoppinglist/service/ShoppingListService.java
+++ backend/src/main/java/com/jromeo/backend/shoppinglist/service/ShoppingListService.java
@@ -1,13 +1,11 @@
 package com.jromeo.backend.shoppinglist.service;
 
-import com.jromeo.backend.provision.dto.ProvisionDTO;
 import com.jromeo.backend.provision.entity.ProvisionEntity;
 import com.jromeo.backend.provision.mapper.ProvisionMapper;
 import com.jromeo.backend.provision.repository.ProvisionRepository;
 import com.jromeo.backend.shoppinglist.dto.ShoppingListDTO;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -23,15 +21,11 @@ public class ShoppingListService {
 
     public ShoppingListDTO addProvisionsToShoppingList() {
         List<ProvisionEntity> provisionEntities = provisionRepository.addProvisionToShoppingList();
-        List<ProvisionDTO> provisionDTOs = new ArrayList<>();
 
-        for (ProvisionEntity provisionEntity : provisionEntities) {
-            provisionDTOs.add(provisionMapper.mapToDTO(provisionEntity));
-        }
+        ShoppingListDTO provisionsToShoppingList = new ShoppingListDTO();
 
-        ShoppingListDTO provisionsToGroceryShoppingList = new ShoppingListDTO();
-        provisionsToGroceryShoppingList.setProvisionsToShoppingList(provisionDTOs);
+        provisionsToShoppingList.setProvisionsToShoppingList(provisionMapper.mapToDTOs(provisionEntities));
 
-        return provisionsToGroceryShoppingList;
+        return provisionsToShoppingList;
     }
 }
