48a3e8b9da763ae6784b18d77209482c9801fb16
johanromeo <johan_romeo123@live.se>
2024-12-04 20:19:13 +0100

    changed port to 80

--- frontend/people.js
+++ frontend/people.js
@@ -1,4 +1,4 @@
-const baseApiUrl = "http://localhost:8080/household-people";
+const baseApiUrl = "http://localhost:80/household-people";
 
 let isEditMode = false; // Flag to determine if we're adding or editing
 let editPersonId = null; // Store the ID of the person being edited
